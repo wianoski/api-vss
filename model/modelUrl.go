41020971805748e060c9218938e80529b19e52f2
Thirafi Wian Anugrah <thirafi.wian@bluebirdgroup.com>
2022-08-30 14:56:08 +0700

    fix: error

--- model/modelUrl.go
+++ model/modelUrl.go
@@ -1,7 +1,11 @@
 package model
 
 import (
-
+	"errors"
+	"fmt"
+	"io"
+	"net/http"
+	"os"
 )
 
 type ServerUrl struct {
@@ -13,5 +17,32 @@ type ServerUrl struct {
 
 var Url = "http://47.252.16.64"
 var UrlPort = 9966
-var UserName = "BlueB"
-var Password = "3d22abdc5e8c9ab21ba13b540a8875f0"
+
+func SetServerApi(ServerUrl string, KeyUrl string, ActionUrl string, Token string, Paramater string) (string){
+	go func() {
+		mux := http.NewServeMux()
+		mux.HandleFunc("/", func(w http.ResponseWriter, r *http.Request) {
+			fmt.Printf("server: %s /\n", r.Method)
+		})
+		server := http.Server{
+			Addr:    fmt.Sprintf(":%d", UrlPort),
+			Handler: mux,
+		}
+		if err := server.ListenAndServe(); err != nil {
+			if !errors.Is(err, http.ErrServerClosed) {
+				fmt.Printf("error running http server: %s\n", err)
+			}
+		} 
+	}()
+	RequestUrl := fmt.Sprintf("%s:%d/%s/%s/%s?%s&%s",Url,UrlPort, ServerUrl,KeyUrl,ActionUrl,Token,Paramater)
+	//						   "http://47.252.16.64:9966/vss/vehicle/getDeviceStatus.action?token=137e98bc3d0247ce80897b8b887bb029&deviceID=bb345"
+	//							     Url:           urlport/serverurl/keyurl/actionurl
+	res,err := http.Get(RequestUrl)
+	resBody, err := io.ReadAll(res.Body)
+	if err != nil {
+		fmt.Printf("error making http request: %s\n", err)
+		os.Exit(1)
+	}
+
+	return string(resBody)
+}
--- type_test/loginFunc.go
+++ type_test/loginFunc.go
@@ -7,6 +7,7 @@ import (
 	"io"
 	"net/http"
 	"os"
+	
 )
 
 type Server struct{
